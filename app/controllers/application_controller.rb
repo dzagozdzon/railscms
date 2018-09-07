27702040f5e0eb8c82b6a4a8ea33d94edd20e850
dzagozdzon <zagozdzond@gmail.com>
2018-09-08 00:40:43 +0200

    Add frontend blocks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :getPageNav 
+  before_action :getPageNav, :getJumbotronBlocks, :getBlocks  
   protect_from_forgery with: :exception 
 
   def getPageNav
     @pageNav = Page.where("menu_display = true && is_published = true").order(order: :asc)
   end 
+
+  def getJumbotronBlocks
+    @jumbotron = Block.where("position = 'jumbotron'")
+  end
+
+  def getBlocks
+    @blocks = Block.where("position = 'block'").order(order: :asc)
+  end
 end
